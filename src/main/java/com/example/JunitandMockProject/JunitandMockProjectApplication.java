fe4c17b3d79276c1a34cd7779fbaecce85399cb4
SylvainK200 <kouemosylvain@gmai.com>
2022-12-04 08:09:47 +0100

    init(project)

---
+++ src/main/java/com/example/JunitandMockProject/JunitandMockProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.JunitandMockProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitandMockProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitandMockProjectApplication.class, args);
+	}
+
+}
